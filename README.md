4f7fcaeba0d01b06e913bf6e91efb3f951ab4835
orson_codes <orson.network@gmail.com>
2016-07-24 01:57:45 -0400

    fixed file not found on fail write

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 Dnc Botty:
 
 A quick script to the dnc emails leadked by wikileak 
-https://wikileaks.org/dnc-emails/
+https://wikileaks.org/dnc-emails
+
+How to use:
+
+pip install -r requirements.txt
+
+To request all the emails you dont have in your data-dir use:
+python dnc_emails.py --use-missing
+
+if you would like to use a range of emailids use:
+python dnc_emails.py --start [START] end --end [END]
+
+the script creates a directory "dnc_emails/emails" where it stores are the 
+json emails. Use --data-dir to specify thee full path to a different locatio
+
+For aysnc requests use --async. Note that if you do use --async then you will not be able to recover the requests that failed in a particular session.
+
+NO warrenty or guarantee. Use at your own risk and all that jazz.
+
+
--- dnc_emails.py
+++ dnc_emails.py
@@ -10,6 +10,8 @@ import time
 import argparse
 
 
+
+
 POOL_SIZE  = 10
 URL = "https://wikileaks.org/dnc-emails/emailid/"
 HOME = os.path.expanduser("~")
@@ -18,51 +20,71 @@ SLEEP = 20
 CHUNK_TO_SLEEP = 100
 
 START = 1
-ASYNC = 0
+ASYNC = False
 MAX_FAIL = 10
+MAX_EMAILS = 19252 # source : https://wikileaks.org/dnc-emails/
+
+
+def detect_missing():
+
+	""" return a generator of all the ids that are not in the emails directory """
+	try:
+		f = os.walk(DNC_DIR).next()
+		# assumes file names are "x.json" where x can be coerced to an int
+	except IOError:
+		return 
+	else:
+		collected = set(int(x.split(".")[0]) for x in f[2])
+	return (ident for ident in xrange(1, MAX_EMAILS) if ident not in collected)
+
 
 
 def main():
-
-	# for some odd reason I cant define this with the other constants
-	MAX_EMAILS = 3
-	# using argparse to let use import start and end emailsids
+	
 
 	parser = argparse.ArgumentParser(description="GET DNC EMAILS.")
 	parser.add_argument("--start", dest="start", default=START,
-	                    help="emailid to start the crawl at")
+						help="emailid to start the crawl at")
 	parser.add_argument("--end", dest="end", default= MAX_EMAILS,
 						help="emailid to end the crawl at")
-	parser.add_argument("--async", dest="async", default=ASYNC, 
+	parser.add_argument("--async", dest="async", const=1, nargs="?", default=ASYNC, 
 						help="1, 0 for use of async request or not respectively."
 						"Use this if you dont care about the order of the requests")
-	parser.add_argument("---data-dir", dest=DNC_DIR, default=DNC_DIR,
+	parser.add_argument("--data-dir", dest=DNC_DIR, default=DNC_DIR,
 						help="Give te directory of where to store the emails")
+	parser.add_argument("--use-missing", dest="use_missing", default=True, const=1, nargs="?",
+						help="True or False. find the ID of the missing emails from data-dir and"\
+							"request those. --start and --end options will be ignored")
 
 	args = parser.parse_args()
-	# set user agent
-	s = requests.session()
-	# being polite : we"ll let wikileks know who we are and where thdey can find more info
-	s.headers.update({"User-Agent": "Requests-- DNCBotty: A collecting emails" 
-						"for a quick project. https://github.com/DNCBotty"})
 
-		
-	# create directory isit not exit
+	# being polite : we"ll let wikileaks know who we are and where they can find more info.
+	s = requests.session()
+	s.headers.update({"User-Agent": "Requests-- DNCBotty: A Collecting emails" 
+						"for a quick project. https://github.com/DNCBotty"})
 	try:
 		if not os.path.exists(DNC_DIR):
 			os.makedirs(DNC_DIR)
-			# 
 	except OSError:
-		print("OSError")
+		print("OSError while trying to create %s. Quitting" %DNC_DIR)
 		sys.exit(1)
 	
-	# create async requests using
+	# create async requests using grequests
 	# using map instead of imap so I can match the response to the emailid
-	# NOTE ( if aysnc then you can"t (that i know of) recover which requests failed )
-	if args.end < MAX_EMAILS:
-		MAX_EMAILS = args.end
+	# NOTE ( if aysnc then you can't (that i know of) recover which requests failed
+	# by matching the file name to the emailID. grequests.map makes it possibe becasuse request are sequential. )
+	if args.use_missing & args.async:
+		raise TypeError("can't gaurentee the order of results with async. So missing will be devilishly deceitful")
+	elif args.use_missing  & ( not args.async ):
+		missing = detect_missing()
+	else:
+		if args.end < MAX_EMAILS:
+			max_emails = args.end
+		if args.start:
+			start = args.start
+		missing = xrange(start, max_emails+1)
 
-	rs = (grequests.get(u, session=s) for  u in (URL + str(i) for i in xrange(START, MAX_EMAILS+1 )))
+	rs = (grequests.get(u, session=s) for u in (URL + str(i) for i in missing))
 	# async requests with grequests.imap, if user wants.
 	if args.async:
 		resp = grequests.map(rs, size=POOL_SIZE)
@@ -70,26 +92,27 @@ def main():
 		resp = grequests.imap(rs, size=POOL_SIZE)
 
 	# loop through responses
-	for (i, r) in zip(xrange(1, MAX_EMAILS+1), resp):
+	for (i, r) in zip(missing, resp):
 		if r.ok:
-			# reset fail_count if responses are "ok" and fail_count > MAX_FAIL
+			# reset fail_count if responses are "ok" and fail_count < MAX_FAIL
 			fail_count = 0
 			json.dump({"data" : r.text}, open(os.path.join(DNC_DIR,  str(i) + ".json"), "w"))
 		else:
-			# simple write the emailID of the request that failed to failed_emails.txt file
+			# simply write the emailID of the request that failed to failed_emails.txt file as backup
 			try:
 				print("Failed to get email with email id {}".format(i), 
-					file=open(os.path.join(DNC_DIR, "failed_emails.txt", "w")))
-			except Exception as e:
-				print(e)
-				# if fail_count emails fail consecutively kill script
+					file=open(os.path.join(DNC_DIR, "failed_emails.txt"), "w"))
+				# .. if  being throlled ??
 				fail_count+=1
 				if fail_count > MAX_FAIL: 
-					sys.exit("responses failed %s consecutive times..Quitinsg" %MAX_FAIL)
+					sys.exit("responses failed %s consecutive times..Quitting" %MAX_FAIL)
+			except Exception as e:
+				print(e)
 			finally:
 				# back-0ff for courtesy 
 				if (i % CHUNK_TO_SLEEP) & (i != 0):
-						time.sleep(SLEEP)
+					time.sleep(SLEEP)
+
 
 if __name__ == "__main__":
 	main()
